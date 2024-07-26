72341a9d99a194c02939aca1f04b5e3f13f163d1
ngobach26 <ngobach26@gmail.com>
2024-07-26 14:49:48 +0700

    authentication

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,23 @@
 module ApplicationHelper
-  def full_title(title = "")
-    base_title = I18n.t "base_title"
+  def full_title(title = '')
+    base_title = I18n.t 'base_title'
     if title.blank?
       base_title
     else
-      title + " | " + base_title
+      title + ' | ' + base_title
+    end
+  end
+
+  def message_case(key)
+    case key
+    when "notice"
+      :success
+    when "info"
+      :info
+    when "warn"
+      :warning
+    when "alert"
+      :danger
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
